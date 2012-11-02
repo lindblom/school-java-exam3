7030346db2ba3e3b36f35aa1c975a649e97dde5d
Christopher Lindblom <lindblom@doless.se>
2012-11-02 09:14:50 +0100

    initial commit

---
+++ VocabularyTrainer.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class VocabularyTrainer here.
+ * 
+ * @author Christopher Lindblom 
+ * @version (a version number or a date)
+ */
+public class VocabularyTrainer
+{
+    /**
+     * Constructor for objects of class VocabularyTrainer
+     */
+    public VocabularyTrainer()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+    
+    public static void main(String[] args)
+    {
+        // run program
+    }
+}
