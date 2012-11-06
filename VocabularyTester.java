465b0a7a80d921aa96f0a617e4f3476c00558732
Christopher Lindblom <lindblom@doless.se>
2012-11-06 20:37:32 +0100

    quick test to store the guesses

--- VocabularyTester.java
+++ VocabularyTester.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Arrays;
 
 /**
  * Write a description of class VocabularyTester here.
@@ -9,6 +11,7 @@ public class VocabularyTester
 {
     // instance variables - replace the example below with your own
     private Vocabulary mVoc;
+    private HashMap<Word, String[]> mGuesses;
 
     /**
      * Constructor for objects of class TestVocabulary
@@ -17,6 +20,7 @@ public class VocabularyTester
     {
         // initialise instance variables
         mVoc = new Vocabulary();
+        mGuesses = new HashMap<Word, String[]>();
     }
     
     public void run()
@@ -27,27 +31,62 @@ public class VocabularyTester
         {
             askWord(word);
         }
-        // print summary!
+        
+        for(Word word : testWords)
+        {
+            System.out.println("Ordet var: " + word.getWord());
+            boolean correct = false;
+            
+            System.out.print("Du gissade: ");
+            for(String guess : mGuesses.get(word))
+            {
+                System.out.print(guess + " ");
+                correct = word.isCorrect(guess);
+            }
+            
+            if(correct)
+            {
+                System.out.print("-- Vilket var rätt!");
+            }
+            else
+            {
+                System.out.print("-- Rätt svar var " + word.getAnswer());
+            }
+        }
     }
     
     private void askWord(Word word)
     {
-        int thisTry = 1;
-        String answer = "";
-        String[] answers = new String[3];
+        int guesses = 1;
+        String guess = "";
         
-        while(thisTry <= 3 && !word.isCorrect(answer))
+        while(guesses <= 3 && !word.isCorrect(guess))
         {
             System.out.println("Hur stavas '" + word.getWord() + "' på engelska?");
-            answer = InputHandler.get();
-            answers[thisTry - 1] = answer;
-            printAnswerFeedback(word.isCorrect(answer));
-            thisTry++;
+            guess = InputHandler.get();
+            addGuess(word, guess);
+            printAnswerFeedback(word.isCorrect(guess));
+            guesses++;
         }
         
-        if(!word.isCorrect(answer))
+        if(!word.isCorrect(guess))
         {
             System.out.println("Det rätta svaret var: " + word.getAnswer());
+        }
+    }
+    
+    private void addGuess(Word inWord, String inGuess)
+    {
+        String[] currentGuesses = mGuesses.get(inWord);
+        if(currentGuesses == null)
+        {
+            String[] guesses = { inGuess };
+            mGuesses.put(inWord, guesses);
+        }
+        else
+        {
+            String[] guesses = Arrays.copyOf(currentGuesses, currentGuesses.length + 1);
+            guesses[guesses.length - 1] = inGuess;
         }
     }
     
