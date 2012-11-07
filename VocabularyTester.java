5ac26952e6f1ab9a8fa302ef070f25957186aae0
Christopher Lindblom <lindblom@doless.se>
2012-11-07 12:02:28 +0100

    in the process of moving code to its own GuessBook class

--- VocabularyTester.java
+++ VocabularyTester.java
@@ -1,5 +1,3 @@
-import java.util.HashMap;
-import java.util.Arrays;
 
 /**
  * Write a description of class VocabularyTester here.
@@ -11,7 +9,7 @@ public class VocabularyTester
 {
     // instance variables - replace the example below with your own
     private Vocabulary mVoc;
-    private HashMap<Word, String[]> mGuesses;
+    private GuessBook mGuessBook;
 
     /**
      * Constructor for objects of class TestVocabulary
@@ -20,7 +18,7 @@ public class VocabularyTester
     {
         // initialise instance variables
         mVoc = new Vocabulary();
-        mGuesses = new HashMap<Word, String[]>();
+        mGuessBook = new GuessBook();
     }
     
     public void run()
@@ -31,7 +29,7 @@ public class VocabularyTester
         {
             askWord(word);
         }
-        
+        /*
         for(Word word : testWords)
         {
             System.out.println("Ordet var: " + word.getWord());
@@ -52,41 +50,26 @@ public class VocabularyTester
             {
                 System.out.print("-- Rätt svar var " + word.getAnswer());
             }
-        }
+        }*/
     }
     
     private void askWord(Word word)
     {
-        int guesses = 1;
+        int thisGuess = 1;
         String guess = "";
         
-        while(guesses <= 3 && !word.isCorrect(guess))
+        while(thisGuess <= 3 && !word.isCorrect(guess))
         {
             System.out.println("Hur stavas '" + word.getWord() + "' på engelska?");
             guess = InputHandler.get();
-            addGuess(word, guess);
+            mGuessBook.addGuess(word, guess);
             printAnswerFeedback(word.isCorrect(guess));
-            guesses++;
+            thisGuess++;
         }
         
         if(!word.isCorrect(guess))
         {
             System.out.println("Det rätta svaret var: " + word.getAnswer());
-        }
-    }
-    
-    private void addGuess(Word inWord, String inGuess)
-    {
-        String[] currentGuesses = mGuesses.get(inWord);
-        if(currentGuesses == null)
-        {
-            String[] guesses = { inGuess };
-            mGuesses.put(inWord, guesses);
-        }
-        else
-        {
-            String[] guesses = Arrays.copyOf(currentGuesses, currentGuesses.length + 1);
-            guesses[guesses.length - 1] = inGuess;
         }
     }
     
